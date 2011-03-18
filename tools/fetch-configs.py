fe99aa46fce0ad0e5f6e310e0cdee3706b318422
Anas Nashif <nashif@linux.intel.com>
2011-03-18 07:28:31 +0000

    add test tools to fetch ks files from repo

---
+++ tools/fetch-configs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import urllib2
+from datetime import date
+from xml.etree.ElementTree import ElementTree
+
+url = "http://download.meego.com/snapshots/1.1.90.8.20110317.88/builddata/image-configs.xml"
+f = urllib2.urlopen(url)
+tree = ElementTree()
+tree.parse(f)
+configs = tree.findall('config')
+for c in configs:
+    planned = False
+    name = c.find('name').text
+    dow = date.today().weekday()
+    if c.find('schedule').text != '':
+        schedule = c.find('schedule').text
+        if schedule == '*' or int(schedule) == dow:
+            planned = True
+    else:
+        print "%s is not scheduled to be created today" %name
+        planned = False
+
